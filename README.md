b0b4bf9d96cc5aa5ed1837ba8e4c3f374a858302
ckarrie <ckarrie@localhost>
2015-12-16 09:15:51 +0100

    add Rec File to output

--- README.md
+++ README.md
@@ -11,6 +11,12 @@ How to use?
 
 create venv
 run pip install -e requirements.txt
-set DVR_LOG_DIR and OUT_FN in ../src/tvhreclist/dvrreclist.py
+set DVR_LOG_DIR, LANG_CODE and OUT_FN in ../src/tvhreclist/dvrreclist.py
 copy files from ~/.hts/config/dvr/logs to ../src/tvhreclist/dvr_log
 run python ../src/tvhreclist/dvrreclist.py
+
+How to sort?
+=====================================================================
+
+Use Libreoffice Calc to import your CSV,
+select the first row then make it sortable by Data > Filter > AutoFilter
--- dvrreclist.py
+++ dvrreclist.py
@@ -6,25 +6,32 @@ import unicodecsv
 DVR_LOG_DIR = '/home/ckw/workspace/src/tvhreclist/dvr_log/*'
 data = []    
 OUT_FN = '/home/ckw/workspace/src/tvhreclist/out.csv'
+LANG_CODE = 'ger'
 
 
 for f in glob.glob(DVR_LOG_DIR):
     with open(f) as fobj:
         f_json = json.loads(fobj.read())
-        title = f_json.get('title', {}).get('ger')
-        subtitle = f_json.get('subtitle', {}).get('ger')
+        title = f_json.get('title', {}).get(LANG_CODE)
+        subtitle = f_json.get('subtitle', {}).get(LANG_CODE)
         fnames = f_json.get('files', [])
         filename = ''
         for fname in fnames:
-            filename = fname.get('filename')
+            _fname = fname.get('filename')
+            if _fname:
+                filename = _fname
 
-        data.append([title, subtitle, filename])
+        data.append([f, title, subtitle, filename])
 
-        print "%s - %s - %s" % (title, subtitle, filename)
+        print "%s = %s - %s - %s" % (f, title, subtitle, filename)
+        if len(fnames) > 1:
+            print " --> Duplicate Recording Files", fnames
 
 with open(OUT_FN, 'w') as foutobj:
     w = unicodecsv.writer(foutobj, encoding='utf-8')
+    # add header col
+    w.writerow(['Rec Log File', 'Title', 'Subtitle', 'Filename'])
     for d in data:
-        w.writerow((d[0], d[1], d[2]))
+        w.writerow((d[0], d[1], d[2], d[3]))
 
 
